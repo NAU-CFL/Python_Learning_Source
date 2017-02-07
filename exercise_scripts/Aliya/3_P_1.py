e41eade903f09eca8a2495e6fb3252e76b80dc9b
kassimova <kassimova.aliya@gmail.com>
2017-02-07 15:58:31 -0600

    3_P_1 is added, Aliya's script

---
+++ exercise_scripts/Aliya/3_P_1.py
@@ -0,0 +1,11 @@
+# Write a Python program that prompts the user for two integer
+# values and displays the result of the first number divided by
+# the second, with exactly two decimal places displayed.
+
+# Author: Aliya Kassimova
+a=int(input("Enter the value: "))
+b=int(input("Enter the value: "))
+result=a/b
+# print(result)
+# print(format(result, '.2f'))
+print("The result is", format(result, '.2f'))
