0bec25f5dafe105c6f113e5accd4456c4c34c275
kassimova <kassimova.aliya@gmail.com>
2017-02-15 17:35:27 -0600

    One more cool thing is added by Aliya

---
+++ exercise_scripts/Aliya/6_P_4.py
@@ -0,0 +1,5 @@
+#Write a Python function named helloWorld that displays
+#"Hello World, my name is ", for any given name passed to the routine.
+
+def helloWorld():
+    input("Hello World, my name is ")  
